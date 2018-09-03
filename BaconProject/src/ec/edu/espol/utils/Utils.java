6b38152c02b7cf10064d813864769f7eced611f2
PincayRosa <rmpincay@espol.edu.ec>
2018-09-02 23:35:49 -0500

    Cambios Sonnar passed

--- BaconProject/src/ec/edu/espol/utils/Utils.java
+++ BaconProject/src/ec/edu/espol/utils/Utils.java
@@ -7,20 +7,17 @@ import ec.edu.espol.tda.PeliculaActor;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Utils {
 
-    public Utils() {
+    private Utils() {
         throw new IllegalStateException("Utility class");
     }
 
